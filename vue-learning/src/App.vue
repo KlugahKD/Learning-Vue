0547d0686791f5cf90e8faa7aabcb7bfcaed8127
KlugahKd <klugahdaniel1@gmail.com>
2024-07-17 21:05:41 +0000

    Job Listing

--- vue-learning/src/App.vue
+++ vue-learning/src/App.vue
@@ -2,15 +2,12 @@
     <Navbar />
     <Hero />
     <HomeCards />
+    <JobListings :limit = 3 :showButton="true" />
 </template>
 
 <script setup>
 import Navbar from '@/components/Navbar.vue';
 import Hero from '@/components/Hero.vue';
 import HomeCards from '@/components/HomeCards.vue';
-
+import JobListings from './components/JobListings.vue';
 </script>
-
-<style scoped>
-
-</style>
---
+++ vue-learning/src/components/JobListings.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="bg-blue-50 px-4 py-10">
+        <div class="container-xl lg:container m-auto">
+            <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+                Browse Jobs
+            </h2>
+            <JobsList v-for="job in jobs.slice(0, limit || jobs.lenght)" 
+            :key="job.id" 
+            :job="job" />
+        </div>
+    </section>
+
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+      <a
+        href="/jobs"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
+</template>
+
+<script setup>
+import jobData from '@/jobs.json';
+import { ref } from 'vue';
+import JobsList from './JobList.vue';
+
+const jobs = ref(jobData.jobs)
+
+const props = defineProps({
+    limit: Number,
+    showButton: Boolean,
+});
+</script>
